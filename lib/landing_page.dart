e84baf890c338e351338a028a8398b8a3a8acafc
Marron <mjfloreschu2004@gmail.com>
2025-05-12 01:11:47 +0800

    SIDE NAV AND LANDING PAGE

---
+++ lib/landing_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'sidenav.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const SideNav(),
+      appBar: AppBar(
+        title: const Text('RNR Fitness Gym App'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: const Center(child: Text('Landing Page Content')),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'landing_page.dart';
+
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RNR App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LandingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
