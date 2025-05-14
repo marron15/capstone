0265107b823116c9c9e56df56ab752d8dcfb35c7
Marron <mjfloreschu2004@gmail.com>
2025-05-14 23:24:23 +0800

    Adding Admin side

---
+++ admin_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const AdminApp());
+}
+
+class AdminApp extends StatelessWidget {
+  const AdminApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
