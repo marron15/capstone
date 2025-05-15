4eec2fb943f5cbb16c5f7a12aa6c641d05dc7bdc
Marron <mjfloreschu2004@gmail.com>
2025-05-15 20:47:26 +0800

    inalis lang yung debug banner

--- admin/lib/main.dart
+++ admin/lib/main.dart
@@ -11,6 +11,7 @@ class AdminApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Admin Panel',
       theme: ThemeData(
         primarySwatch: Colors.blue,
